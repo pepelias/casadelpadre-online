ea3cbea55557da448897bbb859d27df1dd21e0ca
pepelias <j.avellogomez@gmail.com>
2021-08-13 18:31:39 -0400

    Adaptar informacion para optimizar cliente

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -11,48 +12,75 @@ import (
 )
 
 type Stream struct {
-	Online  bool              `json:"online"`
-	Streams map[string]string `json:"streams"`
-	Chat    []*Message        `json:"chat"`
-	Viewers int               `json:"viewers"`
-}
-
-type Message = struct {
-	Username string `json:"username"`
-	Message  string `json:"message"`
+	Online  bool                     `json:"online"`
+	Streams map[string]string        `json:"streams"`
+	Chat    []map[string]interface{} `json:"chat"`
+	Viewers int                      `json:"viewers"`
 }
 
 var (
 	WS     = melody.New()
 	STREAM = &Stream{
+		Online:  true,
 		Streams: make(map[string]string),
-		Chat:    make([]*Message, 0),
+		Chat:    make([]map[string]interface{}, 0),
 	}
 )
 
 func main() {
 	e := echo.New()
 	e.Pre(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000"},
+		AllowOrigins: []string{"http://localhost:3000", "http://192.168.1.135:3000"},
 	}))
 	Router(e)
 	// Websocket
 	WS.HandleMessage(func(sess *melody.Session, msg []byte) {
 		go func() {
-			message := &Message{}
-			err := json.Unmarshal(msg, message)
+			message := make(map[string]interface{})
+			err := json.Unmarshal(msg, &message)
 			if err != nil {
 				log.Println(err)
 				return
 			}
-			STREAM.Chat = append(STREAM.Chat, message)
+			if message["action"] == nil || message["action"].(string) != "chat-message" {
+				return
+			}
+			STREAM.Chat = append(STREAM.Chat, message["data"].(map[string]interface{}))
 		}()
 		WS.Broadcast(msg)
 	})
-
+	WS.HandleConnect(func(s *melody.Session) {
+		STREAM.Viewers += 1
+		go func() {
+			message := map[string]interface{}{
+				"action": "viewers-refresh",
+				"data": map[string]interface{}{
+					"viewers": STREAM.Viewers,
+				},
+			}
+			send(message)
+			time.Sleep(time.Second * 2)
+			sendTo(s, message)
+		}()
+	})
+	WS.HandleDisconnect(func(s *melody.Session) {
+		STREAM.Viewers -= 1
+		send(map[string]interface{}{
+			"action": "viewers-refresh",
+			"data": map[string]interface{}{
+				"viewers": STREAM.Viewers,
+			},
+		})
+	})
 	e.Start(":8080")
 }
 func Router(e *echo.Echo) {
+	e.GET("/v1/streaming/online", func(c echo.Context) error {
+		if STREAM.Online {
+			return c.NoContent(http.StatusOK)
+		}
+		return c.NoContent(http.StatusNotFound)
+	})
 	e.GET("/v1/streaming", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, STREAM)
 	})
@@ -61,4 +89,18 @@ func Router(e *echo.Echo) {
 		return nil
 	})
 	// Endpoints para NGIX
+}
+func send(msg map[string]interface{}) {
+	message, err := json.Marshal(msg)
+	if err != nil {
+		return
+	}
+	WS.Broadcast(message)
+}
+func sendTo(sess *melody.Session, msg map[string]interface{}) {
+	message, err := json.Marshal(msg)
+	if err != nil {
+		return
+	}
+	sess.Write(message)
 }
